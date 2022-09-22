aa5bb85813df2ff67a5fb74475e74a45e225ccb0
Sansone11 <sansonemirco@gmail.com>
2022-09-22 13:50:11 +0200

    structure edit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="nav_container">
+        <nav class="nav_bar">
+            <div class="logo">
+
+            </div>
+            <div class="menu">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main class="container">
+        <div class="text">
+            <?php
+            $faq = [
+                [
+
+                ],
+                [
+
+                ],
+                [
+
+                ],
+                [
+
+                ],
+                [
+                    
+                ]
+            ];
+            ?>
+        </div>
+    </main>
+</body>
+
+</html>
